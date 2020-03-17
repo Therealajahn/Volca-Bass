a8c69e22b97b45f3123debf2a89487fe25fba993
Therealajahn <therealajahn@gmail.com>
2020-03-16 22:04:45 -0500

    put sound setup directly in state initialization, fixed triggering issue

--- src/components/Volca-Bass/VolcaBass.js
+++ src/components/Volca-Bass/VolcaBass.js
@@ -13,41 +13,11 @@ function VolcaBass() {
   //PROPS:
   //knobvalue(object)
   // const[notes, setNotes] = useState(['a1']);
-  const key = useRef();
-  const [sound, setSound] = useState({
-    osc1:null,
-    osc2:null,
-    osc3:null,
-    env: null,
-    filtEnv: null});
-  
-  const letterToNote = {
-    //bottom row
-    z: "c1", s: "c#1", x: "d1", d: "d#1", c: "e1", v: "f1",
-    g: "f#1", b: "g1", h: "g#1", n: "a1", j: "a#1", m: "b1",
-    ',': "c2", l: "c#2", '.': "d2", ';': "d#2",'/': "e2",
-     //top row
-     q: "c2",2: "c#2", w: "d2", 3: "d#2", e: "e2",r: "f2",
-     5: "f#2",t: "g2", 6: "g#2", y: "a2", 7: "a#2", u: "b2",
-     i: "c3", 9: "c#3", o: "d3", 0: "d#3", p: "e3",
-  }
-
- 
-  
-  // const numToNote = {
-  //   1: "c1", 2: "c#1", 3: "d1", 4: "d#1", 5: "e1",
-  //   6: "f1", 7: "f#1", 8: "g1", 9: "g#1", 10: "a1",
-  //   11: "a#1",12: "b1",13: "c2",14: "c#2",15: "d2",16: "d#2",
-  //   17: "e2",18: "f2",19: "f#2",20: "g2",21: "g#2",22: "b2",
-  //   24: "c3",25: "c#3",26: "d3",27: "d#3",28: "e3",29: "f3",
-  //   30: "f#3",31: "g3",32: "g#3",33: "a3",34: "a#3",35: "b3",
-  //   36: "c4",37: "c#4",38: "d4",39: "d#4",40: "e4",
-  // }
-  
-  useEffect(() => {
+  const [key, setKey] = useState();
+  const [sound, setSound] = useState(()=>{
     //setup Sound
     //Setup volume
-        let gain = new Tone.Gain(0.05).toMaster();
+    let gain = new Tone.Gain(0.05).toMaster();
     //Setup filter
         let filterFreq = 200;
         let filter = new Tone.Filter({
@@ -80,17 +50,84 @@ function VolcaBass() {
         let osc1 = new Tone.Oscillator(440,"square").connect(env);
         let osc2 = new Tone.Oscillator(440, "square").connect(env);
         let osc3 = new Tone.Oscillator(440, "square").connect(env);
+  
+       return { osc1:osc1,
+                osc2:osc2,
+                osc3:osc3,
+                env:env,
+                filtEnv:filtEnv }
+      });
+  
+  const letterToNote = {
+    //bottom row
+    z: "c1", s: "c#1", x: "d1", d: "d#1", c: "e1", v: "f1",
+    g: "f#1", b: "g1", h: "g#1", n: "a1", j: "a#1", m: "b1",
+    ',': "c2", l: "c#2", '.': "d2", ';': "d#2",'/': "e2",
+     //top row
+     q: "c2",2: "c#2", w: "d2", 3: "d#2", e: "e2",r: "f2",
+     5: "f#2",t: "g2", 6: "g#2", y: "a2", 7: "a#2", u: "b2",
+     i: "c3", 9: "c#3", o: "d3", 0: "d#3", p: "e3",
+  }
 
-        setSound({
-          osc1:osc1,
-          osc2:osc2,
-          osc3:osc3,
-          env: env,
-          filtEnv: filtEnv
-        })
+ 
+  
+  // const numToNote = {
+  //   1: "c1", 2: "c#1", 3: "d1", 4: "d#1", 5: "e1",
+  //   6: "f1", 7: "f#1", 8: "g1", 9: "g#1", 10: "a1",
+  //   11: "a#1",12: "b1",13: "c2",14: "c#2",15: "d2",16: "d#2",
+  //   17: "e2",18: "f2",19: "f#2",20: "g2",21: "g#2",22: "b2",
+  //   24: "c3",25: "c#3",26: "d3",27: "d#3",28: "e3",29: "f3",
+  //   30: "f#3",31: "g3",32: "g#3",33: "a3",34: "a#3",35: "b3",
+  //   36: "c4",37: "c#4",38: "d4",39: "d#4",40: "e4",
+  // }
+  
+  // useEffect(() => {
+  //   //setup Sound
+  //   //Setup volume
+  //       let gain = new Tone.Gain(0.05).toMaster();
+  //   //Setup filter
+  //       let filterFreq = 200;
+  //       let filter = new Tone.Filter({
+  //           type : 'lowpass',
+  //           frequency : filterFreq,
+  //           rolloff : -12,
+  //           Q : 25,
+  //           gain : 0
+  //       }).connect(gain);
+  //   //Setup env   
+  //       let env = new Tone.AmplitudeEnvelope({
+  //           "attack" : 0.01,
+  //       "decay" : 1,
+  //       "sustain" : 1,
+  //       "release" : 5
+  //       }).connect(filter);
+      
+  //    //Setup filter env      
+  //       let filtEnv = new Tone.FrequencyEnvelope({
+  //           "attack" : 0.01,
+  //           "decay" : 0.1,
+  //           "sustain" : 0.5,
+  //           "release" : 5,
+  //           "baseFrequency" : filterFreq,
+  //           "octaves" : 1,
+  //           "exponent" : 1,
+  //       }).connect(filter.frequency);
+  //   //Setup oscs
+       
+  //       let osc1 = new Tone.Oscillator(440,"square").connect(env);
+  //       let osc2 = new Tone.Oscillator(440, "square").connect(env);
+  //       let osc3 = new Tone.Oscillator(440, "square").connect(env);
+
+  //       setSound({
+  //         osc1:osc1,
+  //         osc2:osc2,
+  //         osc3:osc3,
+  //         env: env,
+  //         filtEnv: filtEnv
+  //       })
       
        
-  },[]);
+  // },[]);
 
  
   async function triggerOrRelease(gate){ 
@@ -113,22 +150,26 @@ function VolcaBass() {
       }
 
       async function triggerSynth(){
-        // console.log("trigger");
-        // const { env, filtEnv } = sound;
-        //   updateOscs(key.current);
-        //   env.triggerAttack();
-        //   filtEnv.triggerAttack();
+        console.log("trigger");
+        const { env, filtEnv } = sound;
+          updateOscs(key);
+          env.triggerAttack();
+          filtEnv.triggerAttack();
       };
         
       async function releaseSynth(){
-      //   console.log("release");
-      // const { env, filtEnv } = sound;
-      //     env.triggerRelease();
-      //     filtEnv.triggerRelease();
+        console.log("release");
+      const { env, filtEnv } = sound;
+          env.triggerRelease();
+          filtEnv.triggerRelease();
       }
         
       async function updateOscs(note){
         const { osc1, osc2, osc3 } = sound;
+        if(!note){
+          note = "c1";
+          console.log('no note');
+        }
           osc1.frequency.value = note;
           osc2.frequency.value = note;
           osc3.frequency.value = note;
@@ -222,7 +263,7 @@ function VolcaBass() {
         function detectMousePress(){
           document.addEventListener("keydown",(e) => {
               e.preventDefault();
-              key.current = letterToNote[e.key];
+              setKey(letterToNote[e.key]);
               triggerOrRelease(1);
              
             });
